0d27b75a03c96ab56096e904dd1b6f6e679b416d
Tanvir19026 <tanvirrafi1999@gmail.com>
2021-04-18 16:33:27 +0600

    done

--- src/components/Home/Packages/Packages.js
+++ src/components/Home/Packages/Packages.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Card,Button} from 'react-bootstrap';
 import { useHistory } from 'react-router';
+import './Package.css';
 
 
 
@@ -18,21 +19,35 @@ const Packages = (props) => {
    
     return (
         <div className="col-md-3 d-flex justify-content-between mb-5 text-center">
-            <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={imageUrl} />
-  <Card.Body>
-    <Card.Title style={{color: 'red'}}>{name}</Card.Title>
-    <Card.Text>
-     <p style={{color:'black',fontWeight:'500'}}>Fee:${price}/Month</p>
-    </Card.Text>
-    <Button variant="primary" onClick={()=>handleClick(name)} >Join Now</Button>
- 
+   
+
+<Card style={{ width: '18rem' }}>
+<Card.Img variant="top" src={imageUrl} />
+<Card.Body>
+  <Card.Title style={{color: 'red'}}>{name}</Card.Title>
+  <Card.Text>
+   <p style={{color:'black',fontWeight:'500'}}>Fee:${price}/Month</p>
+  </Card.Text>
+  <Button variant="primary" onClick={()=>handleClick(name)} >Join Now</Button>
+
+  
+</Card.Body>
+</Card> 
+  
+      
+
+      
     
-  </Card.Body>
-</Card>
 
         </div>
     );
 };
 
 export default Packages;
+
+
+
+
+
+
+
